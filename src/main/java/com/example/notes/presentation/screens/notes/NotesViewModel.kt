d99f2754e5e3281bae8c312a7897d7b44ab4882e
i11usion <i11usiongid@gmail.com>
2025-06-30 22:28:56 +0300

    создание ViewModel для экрана с заметками и тестовая реализация методов

---
+++ src/main/java/com/example/notes/presentation/screens/notes/NotesViewModel.kt
@@ -0,0 +1,99 @@
+@file:Suppress("OPT_IN_USAGE")
+
+package com.example.notes.presentation.screens.notes
+
+import androidx.lifecycle.ViewModel
+import com.example.notes.data.TestNotesRepositoryImpl
+import com.example.notes.domain.AddNoteUseCase
+import com.example.notes.domain.DeleteNoteUseCase
+import com.example.notes.domain.EditNoteUseCase
+import com.example.notes.domain.GetAllNotesUseCase
+import com.example.notes.domain.GetNoteUseCase
+import com.example.notes.domain.Note
+import com.example.notes.domain.SearchNotesUseCase
+import com.example.notes.domain.SwitchPinnedStatusUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+
+class NotesViewModel : ViewModel() {
+
+    private val repository = TestNotesRepositoryImpl
+
+    private val addNoteUseCase = AddNoteUseCase(repository)
+    private val deleteNoteUseCase = DeleteNoteUseCase(repository)
+    private val editNotesUseCase = EditNoteUseCase(repository)
+    private val getAllNotesUseCase = GetAllNotesUseCase(repository)
+    private val getNoteUseCase = GetNoteUseCase(repository)
+    private val searchNotesUseCase = SearchNotesUseCase(repository)
+    private val switchPinnedStatusUseCase = SwitchPinnedStatusUseCase(repository)
+
+    private val query = MutableStateFlow("")
+
+    private val _state = MutableStateFlow(NotesScreenState())
+    val state = _state.asStateFlow()
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    init {
+        query
+            .flatMapLatest {
+                if (it.isBlank()) {
+                    getAllNotesUseCase()
+                } else {
+                    searchNotesUseCase(it)
+                }
+            }
+            .onEach {notesList ->
+                val pinnedNotes = notesList.filter { it.isPinned }
+                val otherNotes = notesList.filter { !it.isPinned }
+                _state.update { it.copy(pinnedNotes = pinnedNotes, otherNotes = otherNotes) }
+            }
+            .launchIn(scope)
+    }
+
+    fun processCommand(command: NotesCommand) {
+        when (command) {
+            is NotesCommand.DeleteNote -> {
+                deleteNoteUseCase(command.noteId)
+            }
+
+            is NotesCommand.EditNote -> {
+                val title = command.note.title
+                editNotesUseCase(command.note.copy(title = "$title edited"))
+            }
+
+            is NotesCommand.InputSearchQuery -> {
+                searchNotesUseCase(command.query)
+            }
+
+            is NotesCommand.SwitchedPinnedStatus -> {
+                switchPinnedStatusUseCase(command.noteId)
+            }
+        }
+    }
+}
+
+sealed interface NotesCommand {
+
+    data class InputSearchQuery(val query: String) : NotesCommand
+
+    data class SwitchedPinnedStatus(val noteId: Int) : NotesCommand
+
+    //Temp
+
+    data class DeleteNote(val noteId: Int) : NotesCommand
+
+    data class EditNote(val note: Note) : NotesCommand
+}
+
+data class NotesScreenState(
+    val query: String = "",
+    val pinnedNotes: List<Note> = emptyList(),
+    val otherNotes: List<Note> = emptyList()
+)
